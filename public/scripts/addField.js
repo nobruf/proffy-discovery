56e360979f007e323d9d1dba8dc4762bb6255b77
nobruf <brunofelipems@outlook.com>
2020-08-14 01:16:23 -0300

    first commit

---
+++ public/scripts/addField.js
@@ -0,0 +1,15 @@
+document.querySelector("#add-time")
+.addEventListener('click', cloneField)
+
+
+function cloneField() {
+    const newFieldsContainer = document.querySelector(".schedule-item").cloneNode(true)
+
+    const fields = newFieldsContainer.querySelectorAll('input')
+   
+    fields.forEach(function(field){
+        field.value=""
+    })
+
+    document.querySelector("#schedule-items").appendChild(newFieldsContainer)
+}
---
+++ src/server.js
@@ -0,0 +1,27 @@
+//server
+const express = require('express')
+
+const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses } = require('./pages')
+
+const nunjucks = require('nunjucks')
+
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
+
+server.use(express.static("public"))
+
+.use(express.urlencoded({extended: true}))
+
+.get("/", pageLanding) 
+ 
+.get("/study", pageStudy)
+
+.get("/give-classes", pageGiveClasses)
+
+.post("/save-classes", saveClasses)
+
+.listen(5500) 
